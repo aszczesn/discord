273d5ea8968140116fd48b664de8c72cf7193130
Albert Szczesny <albert.szczesny@yahoo.com>
2024-12-04 22:54:48 +0000

    Persistance initial implementation

--- todoPrezesy/todoPrezesy_bot.py
+++ todoPrezesy/todoPrezesy_bot.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import discord
 from discord.ext import commands
+import json
 
 # Create a logger for your application
 logger = setup_logger("todoPrezesy")
@@ -17,6 +18,21 @@ if not DISCORD_BOT_TOKEN:
 
 logger.info("Token loaded successfully!")
 
+# Path to the JSON file
+TASKS_FILLE = "tasks.json"
+
+def load_tasks():
+    try:
+        with open(TASKS_FILLE, "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("JSON File not found")
+        return {}
+    
+def save_tasks():
+    with open(TASKS_FILLE, "w") as file:
+        json.dump(todo_lists, file)
+
 # Intents for bot
 intents = discord.Intents.default()
 intents.messages = True
@@ -24,8 +40,8 @@ intents.messages = True
 # Bot prefix
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# In-memory task storage
-todo_lists = {}
+# In-memory task storage, loaded from file
+todo_lists = load_tasks()
 
 @bot.event
 async def on_ready():
@@ -37,7 +53,8 @@ async def add_task(ctx, *, task):
     user = ctx.author.id
     if user not in todo_lists:
         todo_lists[user] = []
-    todo_lists[user].append(task)
+    todo_lists[user].append({"task": task, "completed_by": None, "compleded_at": None})
+    save_tasks()
     await ctx.send(f"Dodano zadanie: {task}")
 
 # Commands to list tasks
@@ -57,8 +74,11 @@ async def complete_task(ctx, task_number: int):
     if user not in todo_lists or len(todo_lists[user]) < task_number or task_number <= 0:
         await ctx.send("A o co chodzi?")
     else:
-        todo_lists[user].pop(task_number - 1)
-        await ctx.send(f"Zrobione: {complete_task}")
+        completed_task = todo_lists[user].pop(task_number - 1)
+        completed_task["completed_by"] = ctx.author.name
+        completed_task["completed_at"] = str(ctx.message.created_at)
+        save_tasks()
+        await ctx.send(f"Zrobione: {completed_task['task']}")
 
 # Run the bot
 bot.run(DISCORD_BOT_TOKEN)
